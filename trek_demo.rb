22b476ea01106362306c64f60900efa05e4aea5e
mikemckinney <mandokir@gmail.com>
2015-04-24 13:27:17 -0700

    updated trek demo

--- trek_demo.rb
+++ trek_demo.rb
@@ -1,17 +1,21 @@
+#trek_demo.rb
+#ruby 2.1.5
+#
+#Initiate program to list, create, modify, and search a hash
 class Officer
   @@crew = {
-  	Worf: "operations",
-        LaForge: "engineering",
-        Picard: "command"
+  	Worf: "Operations",
+        LaForge: "Engineering",
+        Picard: "Command"
 }
 
-  attr_accessor :job
+  attr_accessor :section
   attr_accessor :name
 
-  def initialize(name, job)
+  def initialize(name, section)
 	@name = name
-	@job = job
-        @@crew[name.to_sym] = job
+	@section = section
+        @@crew[name.to_sym] = section
   end
   def self.crew_list
 	@@crew
@@ -20,46 +24,62 @@ class Officer
         @@crew[name.to_sym] = newjob
   end
 end
+
+#Initiate program with menu using while loop
+
+off = ["engineering", "command", "operations"]
+
 ex = ""
 while ex != 5
 
-puts "Contacting Starfleet Command.. "
-puts "Please Enter Selection: "
-puts "1. View crew roster"
-puts "2. Assign new crewman"  
-puts "3. Reassign officer"
-puts "4. Find all officers in a section"
-puts "5. Exit"
-puts "Enter Selection: "
-
+puts "Contacting Starfleet Command.. \n Please Enter Selection: \n 1. View crew roster \n 2. Assign new crewman \n 3. Reassign officer \n 4. Find all officers in a section \n 5. Exit \n Enter Selection: "
 choice = gets.chomp.to_i
 
 
 case choice
-  when  1
-	Officer.crew_list.each do |name, job|
-        puts "#{name} assigned to #{job}." 
+  when 1
+    Officer.crew_list.each do |name, section|
+    puts "#{name} assigned to #{section.upcase}. \n" 
   end
-  when  2
-        puts "Enter officer's name: "
-	   name = gets.chomp
-        puts "Enter assignment, Command, Engineering, or Operations"
-          job = gets.chomp
-          Officer.new(name, job)
-        puts "Officer #{name} has been assigned to #{job}."
 
-  when  3
-	 puts "Enter the name of the officer to reassign: " 
-           name = gets.chomp
-	 puts "What section will #{name} be transferred to?"
-	   section = gets.chomp
-	   Officer.crew_list[name.to_sym] = section.to_s
-	 puts "#{name} has been moved to #{section}."
+  when 2
+    puts "Enter officer's name: "
+      name = gets.chomp.capitalize
+    puts "Enter assignment, Command, Engineering, or Operations: "
+      section = gets.downcase.chomp.to_s
+    if 
+        off.include?(section)
+        Officer.new(name, section)
+        puts "Officer #{name.capitalize} has been assigned to #{section.upcase}. \n"
+    else 
+        puts "Please enter a valid section \n"
+    end
+
+  when 3
+    puts "Enter the name of the officer to reassign: " 
+      name = gets.chomp.capitalize
+    puts "What section will #{name} be transferred to?"
+      section = gets.downcase.chomp.to_s
+    if 
+      off.include?(section) && Officer.crew_list.include?(name.to_sym)
+      Officer.crew_list[name.to_sym] = section
+      puts "#{name.capitalize} has been moved to #{section.upcase}.\n "
+    else 
+      puts "Please enter a valid section \n"
+    end
  
-  when  5
-	puts "Quitting"
-	ex = 5 
-end
+  when 4
+    puts "Enter section to list: "
+      section = gets.chomp
+    Officer.crew_list.each do |name, job|
+    if section == job 
+      puts "#{name} \n"
+    end  
+  end
+  when 5
+    puts "Quitting"
+    ex = 5 
+  end
 end
 
 
